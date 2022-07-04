29e4083ef93f3b25cfb8c127b2e3eeaf65e41069
MrZoeng <493817796@qq.com>
2022-07-04 16:52:04 +0800

    test commit1

--- demo1/src/com/zoeng/po/Customer.java
+++ demo1/src/com/zoeng/po/Customer.java
@@ -44,7 +44,7 @@ public class Customer {
 
     @Override
     public String toString() {
-        return "Customer  [id=" + id + ", username=" + username + ", jobs=" + jobs + ", phone=" + phone + "]";
+        return "Customer  test[id=" + id + ", username=" + username + ", jobs=" + jobs + ", phone=" + phone + "]";
     }
 
 
